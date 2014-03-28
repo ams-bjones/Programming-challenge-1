be13c4590c4e8e111a944f586214fc6db0795693
Brian Jones <bjones@ashmanorschool.com>
2014-03-28 10:24:06 -0400

    precautionary commit

--- multiples.py
+++ multiples.py
@@ -1,2 +1,9 @@
-def multiples(a,b):
-    pass
+def multiplelist(mults,limit):
+    answer = [mults]
+    term = mults
+    while (mults<limit):
+        term = term + mults
+        answer.append(term)
+    return (answer)
+    
+    
