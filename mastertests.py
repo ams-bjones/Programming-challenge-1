4e611c9d8673911b4ee28e3c221e050f30c9ca9b
Brian Jones <bjones@ashmanorschool.com>
2014-03-29 05:17:19 -0400

    multiples added

--- mastertests.py
+++ mastertests.py
@@ -4,7 +4,19 @@ import unittest
 class TestSequenceFunctions(unittest.TestCase):
 
     def test1(self):
-        multiples.multiplelist(5,20)==[5,10,15] #Test 1 fails
+        #print (multiples.multiplelist(5,20))
+        #print ([5,10,15])
+        assert( multiples.multiplelist(5,20)==[5,10,15]) #Test 1 fails
+        
+    def test2(self):
+        inList = [1,2,3]
+        total = 6
+        assert (multiples.totalList(inList) == total) #test 2 fails
+    
+    def test3(self):
+        inlist = multiples.multiplelist(5,20)
+        total = 30
+        assert (multiples.totalList(inlist)==total) #test 3 fails
 
 if __name__ == '__main__':
     unittest.main()
--- multiples.py
+++ multiples.py
@@ -1,9 +1,17 @@
 def multiplelist(mults,limit):
     answer = [mults]
     term = mults
-    while (mults<limit):
+    while (term < (limit-mults)):
         term = term + mults
         answer.append(term)
     return (answer)
     
+def totalList(inlist):
+    answer = 0
+    for something in inlist:
+        answer = answer + something
+    return (answer)
+    
+top = 4000000
+print (totalList(multiplelist(5, top)) + totalList(multiplelist(11,top)))
     
